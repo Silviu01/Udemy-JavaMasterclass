2b546d6abc239f69867e52ef60550614a5cb3e82
Silviu01 <i.silviu_c@yahoo.com>
2018-10-23 18:15:15 +0300

    Section 3, Lecture 17

--- JavaPrograms/HelloWorld/src/Hello.java
+++ JavaPrograms/HelloWorld/src/Hello.java
@@ -1,5 +1,23 @@
+/**
+ * Created by
+ * @author Silviu_Ilinescu on 23-Oct-2018
+ */
+
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, Silviu!");
+
+        int myFirstNumber = 35;
+        int mySecondNumber = 12;
+        int myThirdNumber = 6;
+        int total = myFirstNumber+ mySecondNumber + myThirdNumber;
+
+        int difference = 1000 - total;
+
+        System.out.println(myFirstNumber);
+        System.out.println("myFirstNumber is " + myFirstNumber);
+        System.out.println(difference);
     }
+
+//    Section3, Lecture17
 }
